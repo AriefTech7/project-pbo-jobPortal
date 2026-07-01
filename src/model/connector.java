886a8e882101a846e762b68074b88a953d41bdfb
AriefTech7 <arifwahyudinasution@gmail.com>
2026-07-01 02:53:49 -0700

    connection to database

---
+++ src/model/connector.java
@@ -0,0 +1,11 @@
+package model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class connector {
+    private static Connection MYSQLConfig;
+    
+    public static Connection configDB() throws SQLException{
+        
+    }
+}
